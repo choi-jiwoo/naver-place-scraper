3bb971d81a46a398b38a9d3cc78630f3f4b13e41
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-10-22 15:02:15 +0900

    style: Rename 'browser' to 'driver'

--- snstextscraper/instagram.py
+++ snstextscraper/instagram.py
@@ -13,47 +13,47 @@ ID = config['instagram']['username']
 PW = config['instagram']['password']
 
 # flow 1: login
-browser = webdriver.Chrome("/Users/cho2jiwoo/chromedriver")
-browser.implicitly_wait(1)
-browser.get('https://www.instagram.com/')
+driver = webdriver.Chrome("/Users/cho2jiwoo/chromedriver")
+driver.implicitly_wait(1)
+driver.get('https://www.instagram.com/')
 sleep(1)
 
-username_input = browser.find_element_by_css_selector("input[name='username']")
-password_input = browser.find_element_by_css_selector("input[name='password']")
+username_input = driver.find_element_by_css_selector("input[name='username']")
+password_input = driver.find_element_by_css_selector("input[name='password']")
 
 username_input.send_keys(ID)
 password_input.send_keys(PW)
 
-login_button = browser.find_element_by_xpath("//button[@type='submit']")
+login_button = driver.find_element_by_xpath("//button[@type='submit']")
 login_button.click()
 
 # flow 2: close pop-ups
 for i in range(0, 2):
-    not_not_button = browser.find_element_by_xpath("//button[text()='나중에 하기']")
+    not_not_button = driver.find_element_by_xpath("//button[text()='나중에 하기']")
     not_not_button.click()
     sleep(2)
 
 # flow 3: search hashtag
-search_input = browser.find_element_by_css_selector("input[placeholder='검색']")
+search_input = driver.find_element_by_css_selector("input[placeholder='검색']")
 search_input.send_keys("#애플망고1947")
 sleep(1)
 search_input.send_keys(Keys.DOWN)
 search_input.send_keys(Keys.RETURN)
 
 # flow 4: click on the first post
-imgs_parent = browser.find_element_by_xpath("//img[@class='FFVAD']/parent::div[1]")
+imgs_parent = driver.find_element_by_xpath("//img[@class='FFVAD']/parent::div[1]")
 first_post = imgs_parent.find_element_by_xpath("./following-sibling::div[1]")
 first_post.click()
 
 # flow 5: get post content
-content = browser.find_element_by_xpath("//li[@role='menuitem']")
+content = driver.find_element_by_xpath("//li[@role='menuitem']")
 content_text = content.text
 
 # flow 6: click 'next' button
-next_post = browser.find_element_by_xpath("//a[text()='다음']")
+next_post = driver.find_element_by_xpath("//a[text()='다음']")
 next_post.click()
 
 # loop between flow 5 and flow 6
 
 # flow : close chrome webdriver
-browser.quit()
+driver.quit()
