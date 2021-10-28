8a7517e4237897cdeb6f274c2101cff5769b3e6a
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-10-28 23:24:30 +0900

    refactor: Fit logics into Instagram class and methods

--- snstextscraper/instagram.py
+++ snstextscraper/instagram.py
@@ -1,71 +1,74 @@
 # TO DO: Divide each flows by its functionality.
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-import yaml
 
 
-# configuration
-with open('config.yml', 'r') as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
+class Instagram:
 
-ID = config['instagram']['username']
-PW = config['instagram']['password']
+    def __init__(self, driver: webdriver.Chrome) -> None:
+        self.driver = driver
 
-# flow 1: login
-driver = webdriver.Chrome("/Users/cho2jiwoo/chromedriver")
-driver.get('https://www.instagram.com/')
+    def login(self, id: str, pw: str, timeout: int = 5) -> None:
+        self.driver.get("https://www.instagram.com/")
 
-username_input = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']"))
-)
-password_input = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']"))
-)
+        username_input = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, "input[name='username']")
+            )
+        )
+        password_input = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, "input[name='password']")
+            )
+        )
 
-username_input.send_keys(ID)
-password_input.send_keys(PW)
+        username_input.send_keys(id)
+        password_input.send_keys(pw)
+        submit = self.driver.find_element_by_xpath("//button[@type='submit']")
+        submit.click()
 
-login_button = driver.find_element_by_xpath("//button[@type='submit']")
-login_button.click()
+        try:
+            for i in range(2):
+                self.close_pop_up(timeout)
+        except TimeoutException:
+            print("Login failed. Try again.")
 
-# flow 2: close pop-ups
-for i in range(0, 2):
-    not_now_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, "//button[contains(text(), '나중에 하기')]")  # only works in korean
-        )).click()
+    def close_pop_up(self, timeout: int) -> None:
+        not_now_button = WebDriverWait(self.driver, timeout).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//button[contains(text(), '나중에 하기')]")  # only works in korean
+            )).click()
 
-# flow 3: search hashtag
-hashtag = "애플망고1947"
-driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
+    def search(self, hashtag: str) -> None:
+        self.driver.get(f"https://www.instagram.com/explore/tags/{hashtag}/")
+        first_img_parent_element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//img[@class='FFVAD']/parent::div[1]")  # class name could change
+            )
+        )
+        first_post = first_img_parent_element.find_element_by_xpath(
+            "./following-sibling::div[1]"
+        )
+        sleep(2)
+        first_post.click()
 
-# flow 4: click on the first post
-first_img_parent_element = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable(
-        (By.XPATH, "//img[@class='FFVAD']/parent::div[1]")  # class name could change 
-    )
-)
-first_post = first_img_parent_element.find_element_by_xpath(
-    "./following-sibling::div[1]"
-)
-first_post.click()
+    def get_contents(self) -> dict:
+        contents = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//li[@role='menuitem']"))
+        )
+        author = contents.find_elements_by_tag_name('span')[0].text
+        content_text = contents.find_elements_by_tag_name('span')[1].text
+        post = {author: content_text}
 
-# flow 5: get post content
-content = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//li[@role='menuitem']"))
-)
-content_item = content.find_elements_by_tag_name('span')[1]
-content_text = content_item.text
+        return post
 
-# flow 6: click 'next' button
-next_post = driver.find_element_by_xpath("//a[text()='다음']")  # only works in korean
-next_post.click()
-
-# loop between flow 5 and flow 6
-
-# flow : close chrome webdriver
-driver.quit()
+    def next_post(self) -> None:
+        actions = ActionChains(self.driver)
+        actions.send_keys(Keys.RIGHT)
+        actions.perform()
