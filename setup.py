eda34d8915f14a285ef4199d147f9094f52e6851
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-10-29 11:26:54 +0900

    ci(publish): Improve setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,39 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages
+import os
+import sys
 
 
-DESCRIPTION = ('snstextscraper scrapes text data about the store/company from '
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+
+description = ('Scrapes text data about the store/company from '
                'social network services.')
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+install_requires = [
+    'requests==2.26.0',
+    'selenium==4.0.0',
+]
+
+project_urls = {
+  'Source': 'https://github.com/cho2ji/sns-text-scraper',
+}
 
 setup(
     name='snstextscraper',
-    version='0.0.1',
+    version='0.1.3',
     author='Choi Jiwoo',
     author_email='cho2wldn@gmail.com',
-    description=DESCRIPTION,
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
+    install_requires=install_requires,
     keywords=['python', 'sns', 'scraper', 'crawler'],
     license='MIT',
     classifiers=[
@@ -19,5 +42,6 @@ setup(
         'Programming Language :: Python',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-    ]
+    ],
+    project_urls=project_urls,
 )
