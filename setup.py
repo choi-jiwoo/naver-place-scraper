605f7e2f898b7972bc86af6dd7f4a6bc3d9f467e
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-11-05 04:40:16 +0900

    build: upgrade release version to 0.4.4

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ project_urls = {
 
 setup(
     name='snstextscraper',
-    version='0.4.3',
+    version='0.4.4',
     author='Choi Jiwoo',
     author_email='cho2wldn@gmail.com',
     description=description,
