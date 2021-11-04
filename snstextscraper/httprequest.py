82b12231d6c6fc71d4ead2e479ea27e56ae9014d
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-11-05 03:41:42 +0900

    feat: add User-Agent generation

--- snstextscraper/httprequest.py
+++ snstextscraper/httprequest.py
@@ -1,16 +1,10 @@
 import requests
 import json
+from fake_useragent import UserAgent
 from typing import Optional
 
 
 class HttpRequest:
-
-    headers = {
-        'User-Agent':
-            ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
-             'AppleWebKit/537.36 (KHTML, like Gecko) '
-             'Chrome/94.0.4606.81 Safari/537.36')
-    }
 
     def __init__(self, url: str, type: str = 'get',
                  payload: Optional[str] = None) -> None:
@@ -20,11 +14,16 @@ class HttpRequest:
         self.data = self.request_json()
 
     def request_json(self) -> dict:
+        ua = UserAgent()
+        headers = {
+            'User-Agent': ua.chrome,
+        }
+
         if self.type == 'get':
-            res = requests.get(self.url, headers=HttpRequest.headers)
+            res = requests.get(self.url, headers=headers)
         elif self.type == 'post':
             res = requests.post(self.url,
-                                headers=HttpRequest.headers,
+                                headers=headers,
                                 json=self.payload)
 
         if res.status_code != 200:
