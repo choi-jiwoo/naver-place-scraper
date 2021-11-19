5ca973985f49adad5a4859c5dd8ab3e0a3a0fa31
choi jiwoo <cho2wldn@gmail.com>
2021-11-20 01:58:55 +0900

    refactor: separate get_coordinates to coordinates.py

---
+++ naverplacescraper/coordinates.py
@@ -0,0 +1,24 @@
+from importlib import resources
+from importlib_resources import files
+import data
+
+
+def get_coordinates(location: str) -> tuple:
+        """Get coordinates of a given region.
+
+        :return: Longitude and latitude value of a region.
+        :rtype: tuple
+        """
+        path_to_file = files(data).joinpath('kor_coordinates.txt')
+        coordinates = pd.read_csv(path_to_file, delimiter=',', encoding='euc-kr')
+        coordinates['X'] = coordinates['X'].round(7)
+        coordinates['Y'] = coordinates['Y'].round(7)
+
+        pattern = f'({location}?)'
+        _ = coordinates['SIG_KOR_NM'].str.extract(pattern)
+        _ = _.fillna('')
+        most_relevant = coordinates.iloc[_[0].map(len).idxmax()]
+        longitude = most_relevant['X']
+        latitude = most_relevant['Y']
+
+        return (longitude, latitude)
