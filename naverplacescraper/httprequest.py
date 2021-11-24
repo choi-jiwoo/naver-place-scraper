0a9bb1042eae61aded48e95e07d262532be18c4f
choi jiwoo <cho2.jiwoo@gmail.com>
2021-11-24 23:43:50 +0900

    refactor: separate requesting GET and POST method

--- naverplacescraper/httprequest.py
+++ naverplacescraper/httprequest.py
@@ -1,50 +1,35 @@
 """This module sends HTTP request to a server."""
+from abc import ABC, abstractmethod
 import requests
 import json
 from typing import Optional
 
 
-class HttpRequest:
+class HttpRequest(ABC):
     """A class representing HTTP request.
 
     :param url: Request url.
     :type url: str
-    :param type: Request method. Either 'get or 'post', defaults to 'get'.
-    :type type: str, optional
-    :param payload: Payload to use in POST method, defaults to None.
-    :type payload: Optional[str], optional
-    """
 
+    """
+    
     headers = {
         'User-Agent':
             ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
-             'AppleWebKit/537.36 (KHTML, like Gecko) '
-             'Chrome/94.0.4606.81 Safari/537.36')
+                'AppleWebKit/537.36 (KHTML, like Gecko) '
+                'Chrome/94.0.4606.81 Safari/537.36')
     }
 
-    def __init__(self, url: str, type: str = 'get',
-                 payload: Optional[str] = None) -> None:
+    def __init__(self, url: str, headers: Optional[str] = None) -> None:
         self.url = url
-        self.type = type
-        self.payload = payload
-        self.data = self.request_json()
+        if headers is not None:
+            self.headers = headers
 
-    def request_json(self) -> dict:
-        """Sends HTTP request to a server .
+    @abstractmethod
+    def request(self) -> dict:
+        """Sends HTTP request to a server."""
+        pass
 
-        :return: Response data in dictionary.
-        :rtype: dict
-        """
-        if self.type == 'get':
-            res = requests.get(self.url, headers=HttpRequest.headers)
-        elif self.type == 'post':
-            res = requests.post(self.url,
-                                headers=HttpRequest.headers,
-                                json=self.payload)
-        
-        json_data = self._convert_to_dict(res)
-        
-        return json_data
 
     def _convert_to_dict(self, res: requests.Response) -> dict:
         """Convert JSON response object to dictionary.
@@ -60,4 +45,39 @@ class HttpRequest:
 
         json_data = json.loads(res.text)
 
+        return json_data
+
+
+class Get(HttpRequest):
+    """Sends GET request to a server.
+    """
+
+    def __init__(self, url: str, headers: Optional[str] = None):
+        super().__init__(url, headers)
+        self.response = self.request()
+
+    def request(self):
+        res = requests.get(self.url, headers=self.headers)
+        json_data = self._convert_to_dict(res)
+        
+        return json_data
+
+
+class Post(HttpRequest):
+    """Sends POST request to a server.
+
+    :param payload: Payload to use in POST method, defaults to None.
+    :type payload: Optional[str], optional
+    """
+
+    def __init__(self, url: str, headers: Optional[str] = None,
+                 payload: Optional[str] = None) -> None:
+        super().__init__(url, headers)
+        self.payload = payload
+        self.response = self.request()
+    
+    def request(self) -> dict:
+        res = requests.post(self.url, headers=self.headers, json=self.payload)
+        json_data = self._convert_to_dict(res)
+        
         return json_data
